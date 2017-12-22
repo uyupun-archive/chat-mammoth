1168d127f57772a1464b040f39930ecd06f30d8e
Kazuki Tobita <kazukiti201@gmail.com>
2017-12-22 17:15:46 +0900

    トップページのマークアップ
    fixed #24

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <title>chat mammoth</title>
-    <link rel="stylesheet" href="css/app.css"/>
     <script type="text/javascript">
         window.Laravel = window.Laravel || {};
         window.Laravel.csrfToken = "{{csrf_token()}}";
     </script>
 </head>
 <body>
-    <h1>chat-mammoth</h1>
+    <div class=""><!-- wrapper -->
+        <div class=""><!-- container -->
+            <div class=""><!-- キャッチフレーズ -->
+                hogehoge
+            </div>
+
+            @for ($i = 0; $i < 10; $i++)
+            <div class=""><!-- おすすめのチャットルーム -->
+                <div class="">ルーム名</div><!-- ルーム名 -->
+                <div class=""><!-- タグのwrapper -->
+                    <span>タグ</span><!-- タグ -->
+                    <span>タグ</span><!-- タグ -->
+                    <span>タグ</span><!-- タグ -->
+                </div>
+                <div class=""><!-- 説明のwrapper -->
+                    <p>hoge</p><!-- 説明 -->
+                    <p>piyo</p><!-- 説明 -->
+                </div>
+            </div>
+            @endfor
+
+            <div class=""><!-- チャットルームを探す -->
+                <a href=""></a><!-- リンク -->
+            </div>
+        </div>
+    </div>
 </body>
 </html>
