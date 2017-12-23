228f4f463a7ab9b3a7b146c250c60d392617ba4e
Kazuki Tobita <kazukiti201@gmail.com>
2017-12-24 07:37:55 +0900

    マイページのマークアップ
    fixed #28

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class=""><!-- wrapper -->
+    <div class=""><!-- ユーザ情報 -->
+        <div class="">hoge</div><!-- スクリーンネーム -->
+        <div class="">hoge</div><!-- ユーザID -->
+    </div>
+    <div class="">
+        <div class=""><!-- 作成したルーム -->
+            <div class="">作成したルーム</div><!-- tab -->
+            <div class=""><!-- ルーム -->
+                <div class="">hoge</div><!-- ルーム名 -->
+                <div class="">hogehogehoge</div><!-- description -->
+            </div>
+        </div>
+        <div class=""><!-- お気に入りのルーム -->
+            <div class="">お気に入りのルーム</div><!-- tab -->
+            <div class=""><!-- ルーム -->
+                <div class="">hoge</div><!-- ルーム名 -->
+                <div class="">hogehogehoge</div><!-- description -->
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
