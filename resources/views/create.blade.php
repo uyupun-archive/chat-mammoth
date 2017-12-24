7b0b7ee8ad1556756f97c7664ef131c1be9287ff
Kazuki Tobita <kazukiti201@gmail.com>
2017-12-24 19:15:27 +0900

    blade container修正
    fixed #57

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,29 +2,31 @@
 
 @section('content')
 <div class=""><!-- wrapper -->
-    <h1>ルーム作成</h1><!-- タイトル -->
-    <form method="POST" action="" class="">
-        <div class="">
-            <label for="name" class="">ルーム名</label>
-            <input type="text" name="name" id="name" class="">
-        </div>
-        <div class="">
-            <label for="description" class="">ルームの説明</label>
-            <textarea name="description" id="description" class=""></textarea>
-        </div>
-        <div class="">
-            <label for="tag" class="">タグの追加</label>
-            <input type="text" name="tag" id="tag" class="">
-            <button class="">追加</button>
-        </div>
-        <div class="">
-            <div class="">公開設定</div>
-            <input type="checkbox" name="disclosure" id="disclosure" class="" checked>
-            <label for="disclosure" class="">公開</label>
-            <input type="checkbox" name="disclosure" id="disclosure" class="">
-            <label for="" class="">非公開</label>
-        </div>
-        <button type="submit" class="">ルームを作成</button>
-    </form>
+    <div class=""><!-- container -->
+        <h1>ルーム作成</h1><!-- タイトル -->
+        <form method="POST" action="" class="">
+            <div class="">
+                <label for="name" class="">ルーム名</label>
+                <input type="text" name="name" id="name" class="">
+            </div>
+            <div class="">
+                <label for="description" class="">ルームの説明</label>
+                <textarea name="description" id="description" class=""></textarea>
+            </div>
+            <div class="">
+                <label for="tag" class="">タグの追加</label>
+                <input type="text" name="tag" id="tag" class="">
+                <button class="">追加</button>
+            </div>
+            <div class="">
+                <div class="">公開設定</div>
+                <input type="checkbox" name="disclosure" id="disclosure" class="" checked>
+                <label for="disclosure" class="">公開</label>
+                <input type="checkbox" name="disclosure" id="disclosure" class="">
+                <label for="" class="">非公開</label>
+            </div>
+            <button type="submit" class="">ルームを作成</button>
+        </form>
+    </div>
 </div>
 @endsection
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,23 +2,25 @@
 
 @section('content')
 <div class=""><!-- wrapper -->
-    <div class=""><!-- search wrapper -->
-        <form method="POST" action="" class=""><!-- 検索フォーム -->
-            <input type="text" class=""><!-- テキストボックス -->
-            <button type="button">検索</button>
-        </form>
-    </div>
-    <div class=""><!-- 検索結果wrapper -->
-        <div class="">
-            <div class="">hoge</div><!-- ルーム名 -->
-            <div class=""><!-- タグwrapper -->
-                <span class="">タグ</span><!-- タグ -->
-                <span class="">タグ</span><!-- タグ -->
-                <span class="">タグ</span><!-- タグ -->
-            </div>
-            <div class="">説明</div><!-- description -->
+    <div class=""><!-- container -->
+        <div class=""><!-- search wrapper -->
+            <form method="POST" action="" class=""><!-- 検索フォーム -->
+                <input type="text" class=""><!-- テキストボックス -->
+                <button type="button">検索</button>
+            </form>
         </div>
+        <div class=""><!-- 検索結果wrapper -->
+            <div class="">
+                <div class="">hoge</div><!-- ルーム名 -->
+                <div class=""><!-- タグwrapper -->
+                    <span class="">タグ</span><!-- タグ -->
+                    <span class="">タグ</span><!-- タグ -->
+                    <span class="">タグ</span><!-- タグ -->
+                </div>
+                <div class="">説明</div><!-- description -->
+            </div>
+        </div>
+        <!-- ページネーション -->
     </div>
-    <!-- ページネーション -->
 </div>
 @endsection
