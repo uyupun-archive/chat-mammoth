2c818166f2f28f9df52c6e66875615eb4f548f73
takashi <b7180@oic.jp>
2018-01-21 23:21:24 +0900

    ヘッダーデザイン

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,29 +5,30 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="/css/app.css">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <title>chat mammoth</title>
     </head>
     <body>
         <div id="app">
-            <div class="h-Nav"><!-- ナビゲーションバー -->
-                <div class="h-Container"><!-- container -->
-                    <div class="h-Menu_Left"><!-- メニュー左側 -->
-                        <img src="" class="h-Logo"><!-- ロゴ -->
-                        <a href="{{ url('/') }}">chat mammoth</a>
-                        <a href="" class="h-Search">ルーム検索</a>
-                    </div>
-                    <div class="h-Menu_Right"><!-- メニュー右側 -->
-                        @guest
-                            <a href="{{ route('login') }}">サインイン</a>
-                            <a href="{{ route('register') }}">サインアップ</a>
-                        @else
-                            <a href="" class="h-Create">ルーム作成</a>
-                            <a href="" class="h-Mypage">マイページ</a>
-                            <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">サインアウト</a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
-                        @endguest
-                    </div>
-                </div>
+            <div class="h-Container"><!-- container -->
+                <img src="/images/logo.png" class="h-Logo align-self-center"><!-- ロゴ -->
+                <h1 class="h-Title align-self-center">
+                    <a href="{{ url('/') }}">chat mammoth</a>
+                </h1>
+                <ul class="h-Menu align-self-center"><!-- メニュー -->
+                    @guest
+                        <li class="h-Menu_List"><a href="" class="h-Search">ルーム検索</a></li>
+                        <li class="h-Menu_List"><a href="{{ route('login') }}">サインイン</a></li>
+                        <li class="h-Menu_List"><a href="{{ route('register') }}">サインアップ</a></li>
+                    @else
+                        <li class="h-Menu_List"><a href="" class="h-Search">ルーム検索</a></li>
+                        <li class="h-Menu_List"><a href="" class="h-Create">ルーム作成</a></li>
+                        <li class="h-Menu_List"><a href="" class="h-Mypage">マイページ</a></li>
+                        <li class="h-Menu_List"><a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">サインアウト</a></li>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+                    @endguest
+                </ul>
             </div>
             <div class="st-content"><!-- コンテンツ -->
                 @yield('content')
