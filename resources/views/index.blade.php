6911e7f105b310d6491543cb33fdd4390de95c59
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-05 00:45:38 +0900

    新着のチャットルームが表示されるようにした

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,27 +4,27 @@
 <div class="st-Wrapper">
     <div class="tp-Catchphrase">投げろマサカリ⛏️</div>
     <div class="tp-Container">
-        <h2 class="st-Title">おすすめのチャットルーム</h2>
+        <h2 class="st-Title">新着のチャットルーム</h2>
         <div class="tp-Chatroom_Container">
-            @for ($i = 0; $i < 10; $i++)
-            <div class="tp-Chatroom">
-                <div class="tp-Chatroom_Name">ルーム名</div>
-                <div class="tp-Chatroom_Tag">
-                    <span class="st-Tag">タグ</span>
-                    <span class="st-Tag">タグ</span>
-                    <span class="st-Tag">タグ</span>
+            @foreach($rooms as $room)
+                <div class="tp-Chatroom">
+                    <div class="tp-Chatroom_Name">{{ $room->name }}</div>
+                    <div>{{ $room->room_id }}</div>
+                    <div class="tp-Chatroom_Tag">
+                        <span class="st-Tag">タグ</span>
+                        <span class="st-Tag">タグ</span>
+                        <span class="st-Tag">タグ</span>
+                    </div>
+                    <div class="tp-Chatroom_Description">
+                        <p>{{ $room->description }}</p>
+                    </div>
+                    <div class="tp-Chatroom_LinkBox">
+                        <a href="" class="tp-Chatroom_Link">チャットルームへ</a>
+                    </div>
                 </div>
-                <div class="tp-Chatroom_Description">
-                    <p>hoge</p>
-                    <p>piyo</p>
-                </div>
-                <div class="tp-Chatroom_LinkBox">
-                    <a href="" class="tp-Chatroom_Link">チャットルームへ</a>
-                </div>
-            </div>
-            @endfor
+            @endforeach
             <div class="tp-Search">
-                <a href="{{ url('/search') }}" class="tp-Search_Link">チャットルームを探す</a><
+                <a href="{{ url('/search') }}" class="tp-Search_Link">チャットルームを探す</a>
             </div>
         </div>
     </div>
