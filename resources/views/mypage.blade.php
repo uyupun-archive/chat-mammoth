3521e0a64b9ce0c465be958c995a3e85e297ea4b
takashi <b7180@oic.jp>
2018-02-05 12:13:08 +0900

    マイページ修正

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -6,19 +6,19 @@
             <h2 class="st-Title">マイページ</h2>
             <div class="mp-Information">
                 <div class="mp-Information_Name">{{ $screen_name }}</div>
-                <div class="mp-Information_UserId">{{ $user_id }}</div>
+                <div class="mp-Information_UserId">@ {{ $user_id }}</div>
             </div>
             <div class="mp-Nav_Area">
                 <ul class="nav nav-tabs" id="myTab" role="tablist">
-                    <li class="nav-item">
-                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#create" role="tab" aria-controls="create" aria-selected="true">作成したルーム</a>
+                    <li class="nav-item active">
+                        <a class="nav-link" data-toggle="tab" href="#create" role="tab" aria-controls="create" aria-expanded="true">作成したルーム</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#favorite" role="tab" aria-controls="favorite" aria-selected="false">お気に入りのルーム</a>
+                        <a class="nav-link" data-toggle="tab" href="#favorite" role="tab" aria-controls="favorite" aria-expanded="false">お気に入りのルーム</a>
                     </li>
                 </ul>
                 <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="create" role="tabpanel" aria-labelledby="create-tab">
+                    <div class="tab-pane active" id="create" role="tabpanel">
                         @foreach($rooms as $room)
                             <div class="mp-Chatroom">
                                 <div class="mp-Chatroom_Name">ルーム名: {{ $room->name }}</div>
@@ -38,7 +38,7 @@
                             </div>
                         @endforeach
                     </div>
-                    <div class="tab-pane fade" id="favorite" role="tabpanel" aria-labelledby="favorite-tab">
+                    <div class="tab-pane" id="favorite" role="tabpanel">
                         <div class="mp-Chatroom">
                             <div class="mp-Chatroom_Name">ルーム名</div>
                             <div class="mp-Chatroom_Tag">
