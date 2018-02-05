9df7214728a4583269fb49df64647b5d4bb7f17e
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-05 10:42:12 +0900

    匿名投稿に対応

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -22,9 +22,12 @@ class RoomController extends Controller {
     public function store(Request $request) {
         $room_id = explode('/', $request->room_id);
 
+        $user_id = isset(Auth::user()->user_id) ? Auth::user()->user_id : 'Anonymous';
+        $screen_name = isset(Auth::user()->screen_name) ? Auth::user()->screen_name : 'Anonymous';
+
         $post = new Post();
-        $post->user_id = Auth::user()->user_id;
-        $post->screen_name = Auth::user()->screen_name;
+        $post->user_id = $user_id;
+        $post->screen_name = $screen_name;
         $post->room_id = $room_id[1];
         $post->comment = $request->comment;
         $post->save();
