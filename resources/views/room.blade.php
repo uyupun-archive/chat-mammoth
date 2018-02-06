fbdc2d76864c31d40c96b4a926ed162a5c9d93c5
takashi <b7180@oic.jp>
2018-02-07 04:41:28 +0900

    コメントが０の場合の文言

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -3,21 +3,25 @@
 @section('content')
     <div class="st-Wrapper">
         <div class="rp-Container">
-            @foreach($posts as $post)
-                <div class="rp-List">
-                    <div class="rp-Message">
-                        <div class="rp-Message_User">
-                            <span class="rp-Message_Name">{{ $post->screen_name }}</span>
-                            <span class="rp-Message_Id">&#64;{{ $post->user_id }}</span>
-                            @if($post->user_id === 'Anonymous')
-                                <i class="fas fa-user-secret"></i>
-                            @endif
-                            <span class="rp-Message_Time">{{ str_replace('-', '/', $post->created_at) }}</span>
+            @if(count($posts) > 0)
+                @foreach($posts as $post)
+                    <div class="rp-List">
+                        <div class="rp-Message">
+                            <div class="rp-Message_User">
+                                <span class="rp-Message_Name">{{ $post->screen_name }}</span>
+                                <span class="rp-Message_Id">&#64;{{ $post->user_id }}</span>
+                                @if($post->user_id === 'Anonymous')
+                                    <i class="fas fa-user-secret"></i>
+                                @endif
+                                <span class="rp-Message_Time">{{ str_replace('-', '/', $post->created_at) }}</span>
+                            </div>
+                            <p>{!! nl2br(htmlspecialchars($post->comment)) !!}</p>
                         </div>
-                        <p>{!! nl2br(htmlspecialchars($post->comment)) !!}</p>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            @else
+                <p>コメントしてね</p>
+            @endif
             <div class="rp-pagination">{{ $posts->links() }}</div>
         </div>
         <div id="accordion" class="rp-Accordion">
