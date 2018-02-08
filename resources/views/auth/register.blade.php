6df930ca21aeb4658199242671feb0df65c179f7
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-08 14:08:27 +0900

    不要なコメントを削除]

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,40 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="st-Wrapper"><!-- wrapper -->
+<div class="st-Wrapper">
     <div class="st-Container">
-        <h1 class="st-Title">サインアップ</h1><!-- タイトル -->
-        <form method="POST" action="{{ route('register') }}" class="su-Form"><!-- 登録フォーム -->
+        <h1 class="st-Title">サインアップ</h1>
+        <form method="POST" action="{{ route('register') }}" class="su-Form">
             {{ csrf_field() }}
-            <div class="su-ScreenName row"><!-- スクリーンネーム -->
-                <label for="screen-name" class="su-ScreenName_Title align-self-center">スクリーンネーム</label><!-- ラベル -->
+            <div class="su-ScreenName row">
+                <label for="screen-name" class="su-ScreenName_Title align-self-center">スクリーンネーム</label>
                 <div class="col">
-                    <input id="screen-name" type="text" name="screen_name" value="{{ old('screen-name') }}" class="su-ScreenName_Textbox form-control" required autofocus><!-- テキストボックス -->
+                    <input id="screen-name" type="text" name="screen_name" value="{{ old('screen-name') }}" class="su-ScreenName_Textbox form-control" required autofocus>
                 </div>
             </div>
-            <div class="su-Id row"><!-- ID -->
-                <label for="user_id" class="su-Id_Title align-self-center">ユーザID</label><!-- ラベル -->
+            <div class="su-Id row">
+                <label for="user_id" class="su-Id_Title align-self-center">ユーザID</label>
                 <div class="col">
-                    <input id="user_id" type="text" name="user_id" value="{{ old('user_id') }}" class="su-Id_Textbox form-control" required autofocus><!-- テキストボックス -->
+                    <input id="user_id" type="text" name="user_id" value="{{ old('user_id') }}" class="su-Id_Textbox form-control" required autofocus>
                 </div>
             </div>
-            <div class="su-Password row"><!-- パスワード -->
-                <label for="password" class="su-Password_Title align-self-center">パスワード</label><!-- ラベル -->
+            <div class="su-Password row">
+                <label for="password" class="su-Password_Title align-self-center">パスワード</label>
                 <div class="col">
-                    <input id="password" type="password" name="password" class="su-Password_Textbox form-control" required><!-- テキストボックス -->
+                    <input id="password" type="password" name="password" class="su-Password_Textbox form-control" required>
                     @if ($errors->has('password'))
-                        <span class="">{{ $errors->first('password') }}</span><!-- バリデーションメッセージ -->
+                        <span class="">{{ $errors->first('password') }}</span>
                     @endif
                 </div>
             </div>
-            <div class="su-Confirm row"><!-- パスワード（確認） -->
-                <label for="password-confirm" class="su-Confirm_Title align-self-center">パスワード（確認）</label><!-- ラベル -->
+            <div class="su-Confirm row">
+                <label for="password-confirm" class="su-Confirm_Title align-self-center">パスワード（確認）</label>
                 <div class="col">
-                    <input id="password-confirm" name="password_confirmation" type="password" class="su-Confirm_Textbox form-control" required><!-- テキストボックス -->
+                    <input id="password-confirm" name="password_confirmation" type="password" class="su-Confirm_Textbox form-control" required>
                 </div>
             </div>
             <div class="su-Button_Area">
-                <button type="submit" class="st-Button su-Button">サインアップ</button><!-- ボタン -->
+                <button type="submit" class="st-Button su-Button">サインアップ</button>
             </div>
         </form>
     </div>
