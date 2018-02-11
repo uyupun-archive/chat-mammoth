94ba72b964b665e0831e87d8e6738a0202ffeb4e
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-11 20:59:08 +0900

    Vueコンポーネント作成

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,6 +4,7 @@ Vue.component('newRoom', require('./components/NewRoom.vue'));
 Vue.component('myPage', require('./components/MyPage.vue'));
 Vue.component('search', require('./components/Search.vue'));
 Vue.component('create', require('./components/Create.vue'));
+Vue.component('gif', require('./components/Gif.vue'));
 Vue.component('handWrite', require('./components/HandWrite.vue'));
 
 const app = new Vue({
