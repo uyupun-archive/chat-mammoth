5c807a66ebd896d3d33ce8b9665bfbd88059255d
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-13 03:20:40 +0900

    Vue 階層整理

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,9 +5,9 @@ Vue.component('myCreate', require('./components/mypage/Create.vue'));
 Vue.component('myFavorite', require('./components/mypage/Favorite.vue'));
 Vue.component('search', require('./components/Search.vue'));
 Vue.component('create', require('./components/Create.vue'));
-Vue.component('keyboard', require('./components/Keyboard.vue'));
-Vue.component('photo', require('./components/Photo.vue'));
-Vue.component('handWrite', require('./components/HandWrite.vue'));
+Vue.component('keyboard', require('./components/room/Keyboard.vue'));
+Vue.component('photo', require('./components/room/Photo.vue'));
+Vue.component('handWrite', require('./components/room/HandWrite.vue'));
 
 const app = new Vue({
     el: '#app'
