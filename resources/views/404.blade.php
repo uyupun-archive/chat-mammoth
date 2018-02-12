7bd8a279993f439e3ffe2fbc738c7f46b2917f17
むらとお <murato@murakamitooru-no-MacBook-Pro.local>
2018-02-12 23:34:39 +0900

    404html
    git commit -m 404html

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,5 +1,5 @@
-<img src="../../../image/caht-mammoth-logo.png" align="caht-mamoth-logo">
+<img src="{{ url('/images/logo3.png') }}" align="caht-mamoth-logo">
 <h1>404 not Found</h1>
 <h3>このページはすでに削除されたか存在しませんでした。</h3>
 <h4><a href="http://127.0.0.1:8000">Top</a></h4>
-<h4><a href="https://github.com/AfterSchoolStudy/chat-mammoth"></a></h4>
+<h4><a href="https://github.com/AfterSchoolStudy/chat-mammoth">Github</a></h4>
