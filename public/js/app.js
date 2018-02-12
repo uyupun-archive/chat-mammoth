26234f5da27aca4104e51823ca9e611eb2bb3a42
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-13 00:39:10 +0900

    画像選択修正

--- public/js/app.js
+++ public/js/app.js
@@ -1308,6 +1308,7 @@ Vue.component('myFavorite', __webpack_require__(90));
 Vue.component('search', __webpack_require__(53));
 Vue.component('create', __webpack_require__(56));
 Vue.component('keyboard', __webpack_require__(59));
+Vue.component('photo', __webpack_require__(96));
 Vue.component('handWrite', __webpack_require__(62));
 
 var app = new Vue({
@@ -45743,6 +45744,144 @@ if (false) {
   module.hot.accept()
   if (module.hot.data) {
     require("vue-hot-reload-api")      .rerender("data-v-28bf3b6e", module.exports)
+  }
+}
+
+/***/ }),
+/* 93 */,
+/* 94 */,
+/* 95 */,
+/* 96 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var disposed = false
+var normalizeComponent = __webpack_require__(1)
+/* script */
+var __vue_script__ = __webpack_require__(97)
+/* template */
+var __vue_template__ = __webpack_require__(98)
+/* template functional */
+var __vue_template_functional__ = false
+/* styles */
+var __vue_styles__ = null
+/* scopeId */
+var __vue_scopeId__ = null
+/* moduleIdentifier (server only) */
+var __vue_module_identifier__ = null
+var Component = normalizeComponent(
+  __vue_script__,
+  __vue_template__,
+  __vue_template_functional__,
+  __vue_styles__,
+  __vue_scopeId__,
+  __vue_module_identifier__
+)
+Component.options.__file = "resources/assets/js/components/Photo.vue"
+
+/* hot reload */
+if (false) {(function () {
+  var hotAPI = require("vue-hot-reload-api")
+  hotAPI.install(require("vue"), false)
+  if (!hotAPI.compatible) return
+  module.hot.accept()
+  if (!module.hot.data) {
+    hotAPI.createRecord("data-v-14d95d8b", Component.options)
+  } else {
+    hotAPI.reload("data-v-14d95d8b", Component.options)
+  }
+  module.hot.dispose(function (data) {
+    disposed = true
+  })
+})()}
+
+module.exports = Component.exports
+
+
+/***/ }),
+/* 97 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+    data: function data() {
+        return {
+            state: true,
+            image: ''
+        };
+    },
+
+    methods: {
+        changeState: function changeState() {
+            this.state = false;
+        }
+    }
+});
+
+/***/ }),
+/* 98 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div", [
+    _c("label", { staticClass: "rp-File_Area", attrs: { for: "file-area" } }, [
+      _c("i", { staticClass: "fas fa-cloud-upload-alt fa-4x rp-Cloud_Upload" }),
+      _vm._v(" "),
+      _c("p", { staticClass: "rp-File_Text" }, [_vm._v("画像を選択")]),
+      _vm._v(" "),
+      _c("input", {
+        staticClass: "rp-File_Input mr-auto",
+        attrs: {
+          type: "file",
+          name: "image",
+          required: "",
+          accept: "image/*",
+          id: "file-area"
+        },
+        on: {
+          change: function($event) {
+            _vm.changeState()
+          }
+        }
+      })
+    ]),
+    _vm._v(" "),
+    _c("div", { staticClass: "rp-Form_Other d-flex" }, [
+      _c(
+        "button",
+        {
+          staticClass: "st-Button rp-Button",
+          attrs: { type: "submit", disabled: _vm.state }
+        },
+        [_vm._v("投稿する")]
+      )
+    ])
+  ])
+}
+var staticRenderFns = []
+render._withStripped = true
+module.exports = { render: render, staticRenderFns: staticRenderFns }
+if (false) {
+  module.hot.accept()
+  if (module.hot.data) {
+    require("vue-hot-reload-api")      .rerender("data-v-14d95d8b", module.exports)
   }
 }
 
