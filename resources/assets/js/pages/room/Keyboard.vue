c527238f8d5d13c9e52e7c9c1287dba9e1224e58
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-17 16:11:45 +0900

    command + enterで送信

--- resources/assets/js/pages/room/Keyboard.vue
+++ resources/assets/js/pages/room/Keyboard.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <textarea name="comment" placeholder="Please write here." class="rp-TextArea" id="rp-TextArea" autofocus required minlength="1" maxlength="1000" v-model="text" @input="changeState()"></textarea>
+        <textarea name="comment" placeholder="Please write here." class="rp-TextArea" id="rp-TextArea" autofocus required minlength="1" maxlength="1000" v-model="text" @input="changeState()" @keydown="sendHandler($event)"></textarea>
         <div class="rp-Form_Other d-flex">
             <input type="checkbox" id="markdown" name="markdown">
             <label class="rp-Markdown" for="markdown">マークダウン</label>
@@ -14,13 +14,18 @@
         data() {
             return {
                 state: true,
-                text: ''
+                text: '',
             }
         },
         methods: {
             changeState() {
                 this.text.length > 0 ? this.state = false : this.state = true
             },
+            sendHandler(e) {
+                if ((e.metaKey || e.ctrlKey) && e.keyCode == 13) {
+                    e.target.form.submit()
+                }
+            }
         },
     }
 </script>
