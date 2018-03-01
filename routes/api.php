8fe97ad808e6e5ab867cef9b1c9f8ecb6cde0a80
Kazuki Tobita <kazukiti201@gmail.com>
2018-03-01 22:56:20 +0900

    ルーティング修正

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/room/get', 'IndexController@get');
+Route::post('/room/search', 'SearchRoomsController@search');
+Route::get('/mypage/create', 'MypageController@create');
+Route::get('/mypage/favorite', 'MypageController@favorite');
+Route::post('/comment/text', 'RoomController@text');
+Route::post('/comment/image', 'RoomController@image');
+Route::post('/comment/gif', 'RoomController@gif');
+Route::post('/comment/draw', 'RoomController@draw');
+Route::get('/word/get', 'BotController@getWord');
+Route::post('/room/store', 'CreateRoomsController@store');
+Route::post('/user/update', 'ConfigController@update');
+Route::post('/favorite/post', 'FavoriteController@post');
+Route::get('/favorite/get', 'FavoriteController@get');
--- routes/web.php
+++ routes/web.php
@@ -4,39 +4,24 @@ Auth::routes();
 
 /** トップ */
 Route::get('/', 'IndexController@index');
-Route::get('/api/room/get', 'IndexController@get');
 
 /** チャットルーム検索 */
 Route::get('/search', 'SearchRoomsController@index');
-Route::post('/api/room/search', 'SearchRoomsController@search');
 
 /** マイページ */
 Route::get('/mypage', 'MypageController@index');
-Route::get('/api/mypage/create', 'MypageController@create');
-Route::get('/api/mypage/favorite', 'MypageController@favorite');
 
 /** チャットルーム */
 Route::get('/room/{id}', 'RoomController@index');
-Route::post('/api/comment/text', 'RoomController@text');
-Route::post('/api/comment/image', 'RoomController@image');
-Route::post('/api/comment/gif', 'RoomController@gif');
-Route::post('/api/comment/draw', 'RoomController@draw');
 
 /** Bot */
 Route::post('/api/comment/bot', 'BotController@postComment');
-Route::get('/api/word/get', 'BotController@getWord');
 
 /** チャットルーム作成 */
 Route::get('/create', 'CreateRoomsController@index');
-Route::post('/api/room/store', 'CreateRoomsController@store');
 
 /** 設定 */
 Route::get('/config', 'ConfigController@index');
-Route::post('/api/user/update', 'ConfigController@update');
-
-/** お気に入り */
-Route::post('/api/favorite/post', 'FavoriteController@post');
-Route::get('/api/favorite/get', 'FavoriteController@get');
 
 /** エラー */
 Route::get('/404', 'ErrorsController@status404');
